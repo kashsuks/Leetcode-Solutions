34aea592d1e1e106ab32a45600ddced34df20988
Kashyap Sukshavasi <kashyapsukshavasi@gmail.com>
2024-04-21 15:34:37 -0400

    Create PalindromeNumber.py

---
+++ PalindromeNumber.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+        xStr = str(x)
+        arr = []
+        for i in xStr:
+            arr.append(i)
+        if arr == arr[::-1]:
+            return True
+        return False
